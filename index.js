159b914d95a065d62bd8d915103adefcf8ae91e7
cris <cris@dosy>
2017-08-31 12:04:26 +0000

    Made dosyhil components
    Closes #1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 {
   const path = require('path');
+  const views = require('./views.js');
   const express = require('express');
   let app;
 
@@ -9,8 +10,23 @@
   function start() {
     app = express();
     app.use('/', express.static(path.join(__dirname, './')))
+    define_handlers();
     app.listen(8080, () => {
       console.log("Server started...");
+    });
+  }
+
+  function define_handlers() {
+    app.get('/build', async (req,res,next) => {
+      res.type('html');
+      const html = await views.build({});
+      res.end(html);
+    });
+
+    app.get('/search', async (req,res,next) => {
+      res.type('html');
+      const html = await views.search({});
+      res.end(html);
     });
   }
 }
