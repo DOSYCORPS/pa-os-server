5c0ef7707d0797ffa76214c64c9b66203d1ad099
Cris <writetocris@outlook.com>
2017-09-26 10:19:03 +0000

    Gutted comms out of here

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,5 @@
 "use strict";
 {
-  const sg = top.require('selector-generalization');
-  const rp = top.require('request-promise');
-
   setup();
 
   function setup() {
@@ -10,17 +7,5 @@
   }
 
   function install() {
-    const generalize_action = document.querySelector('#generalize'); 
-    generalize_action.addEventListener('click', e => recalculate(e));
-  }
-
-  async function recalculate(e) {
-    e.preventDefault();
-    const generalized = document.querySelector('#generalized'); 
-    const ngeneralized = document.querySelector('#ngeneralized'); 
-    const db = JSON.parse(await rp('http://localhost:8080/db'));
-    const result = sg.generalize( db.prop.locations, db.prop.nlocations, true );
-    generalized.value = `${result.positive}`;
-    ngeneralized.value = `${result.negative}`;
   }
 }
